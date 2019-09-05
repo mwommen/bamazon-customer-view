e2e56ff6b85c028d829410d10959c9516b88546e
Mike Ommen <mike@Mikes-MacBook-Pro.local>
2019-09-05 12:01:55 -0500

    Adding the manager.js file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # bamazon-customer-view
+
+Dependencies:
+Install mySQL
+Install inquirer
+
+Use mySQL Workbench to create database and table 
+
+
+In the customer module, the user will be able to select a number of different products based on their ID number. 
+After selecting the item, the user inputs the number of itemsthey wish to purchase.
+If the stock quantity is greater than the number of items being purchased, add the total for the user and decrease the stock quantity
+If the stock quantity is less than the number of items being purchased, aler the user of insufficient stock available to sell. 
+Promt user to select a small amount or different item. 
+
+
+
---
+++ bamazonManager.js
@@ -0,0 +1,53 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// Connects to the database.
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "cardinals1",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+if(err) throw err;
+
+managerSelection();
+});
+
+var managerSelection = function() {
+    inquirer.prompt([
+        {
+        type:'list',
+        name: 'action',
+        message: 'what would you like to do',
+        choices: ['View products for sale',
+                    'View low Inventory',
+                    'Add to Inventory',
+                    'Add new Product' ]
+}
+
+
+]).then (function(answer) {
+    switch (answer.action) {
+        case "View Products for Sale":
+            viewProducts();
+            break;
+
+        case "View Low Inventory":
+            viewLowInventory();
+            break;
+
+        case "Add to Inventory":
+            addToInventory(); 
+            break;
+
+        case "Add New Product":
+            addProduct();
+            break; 
+    }
+});
+
+
+};
